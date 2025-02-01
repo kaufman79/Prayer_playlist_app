919b02dee26fc44992c3b994c5d7775e1db41fb5
kaufman79 <kaufman79@gmail.com>
2025-01-31 22:13:58 -0500

    add and remove prayers

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import sqlite3
 import random
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit, QFormLayout
 
 
 # Database setup
@@ -25,7 +25,7 @@ class PrayerApp(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Prayer App")
-        self.setGeometry(100, 100, 400, 300)
+        self.setGeometry(100, 100, 400, 400)
         self.layout = QVBoxLayout()
 
         self.label_name = QLabel("Click Next to begin")
@@ -38,11 +38,26 @@ class PrayerApp(QWidget):
         self.button_prayed = QPushButton("Mark as Prayed")
         self.button_prayed.clicked.connect(self.mark_as_prayed)
 
+        self.name_input = QLineEdit()
+        self.topic_input = QLineEdit()
+        self.button_add = QPushButton("Add Prayer")
+        self.button_add.clicked.connect(self.add_prayer)
+
+        self.button_delete = QPushButton("Delete Prayer")
+        self.button_delete.clicked.connect(self.delete_prayer)
+
+        form_layout = QFormLayout()
+        form_layout.addRow("Name:", self.name_input)
+        form_layout.addRow("Prayer Topic:", self.topic_input)
+
         self.layout.addWidget(self.label_name)
         self.layout.addWidget(self.label_topic)
         self.layout.addWidget(self.label_last_prayed)
         self.layout.addWidget(self.button_next)
         self.layout.addWidget(self.button_prayed)
+        self.layout.addLayout(form_layout)
+        self.layout.addWidget(self.button_add)
+        self.layout.addWidget(self.button_delete)
 
         self.setLayout(self.layout)
         self.current_prayer = None
@@ -76,6 +91,31 @@ class PrayerApp(QWidget):
             conn = sqlite3.connect("prayers.db")
             cursor = conn.cursor()
             cursor.execute("UPDATE people SET last_prayed = DATE('now') WHERE id = ?", (self.current_prayer[0],))
+            conn.commit()
+            conn.close()
+            self.load_next_prayer()
+
+    def add_prayer(self):
+        name = self.name_input.text()
+        topic = self.topic_input.text()
+        if name and topic:
+            conn = sqlite3.connect("prayers.db")
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO people (name, last_prayed) VALUES (?, NULL)", (name,))
+            person_id = cursor.lastrowid
+            cursor.execute("INSERT INTO prayer_topics (person_id, topic) VALUES (?, ?)", (person_id, topic))
+            conn.commit()
+            conn.close()
+            self.name_input.clear()
+            self.topic_input.clear()
+            self.load_next_prayer()
+
+    def delete_prayer(self):
+        if self.current_prayer:
+            conn = sqlite3.connect("prayers.db")
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM prayer_topics WHERE person_id = ?", (self.current_prayer[0],))
+            cursor.execute("DELETE FROM people WHERE id = ?", (self.current_prayer[0],))
             conn.commit()
             conn.close()
             self.load_next_prayer()
